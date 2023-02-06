1bfcae8f718481d443182fd834ed07448e50482a
Pedro Rabelo de Freitas <51888825+PdRabelo@users.noreply.github.com>
2023-02-05 22:37:34 -0300

    release files

--- src/components/header/index.tsx
+++
@@ -1,8 +0,0 @@
-import "./index.css"
-
-function Header(){
-    return <header className="linguz-header">
-        <img src="logo.svg" width="60px" height="60px"></img>
-    </header>
-}
-export default Header;
